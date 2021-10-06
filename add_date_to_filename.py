b14601d8ccf5cdc924bad611d5ee96e31db98c4a
Petur Jokull Thorvaldsson <33139856+peturjokull@users.noreply.github.com>
2021-10-06 22:12:33 +0000

    run if you like ordering and searching for files by date

---
+++ add_date_to_filename.py
@@ -0,0 +1,18 @@
+import os
+from time import localtime,strftime
+
+path = input(f"Paste your path in here: (C:\\example\\example) \n")
+current_directory = os.getcwd()
+if path == current_directory: pass
+else : current_directory = path 
+
+files_in_dir = os.listdir(current_directory)
+
+for file in files_in_dir:
+    creation_date = strftime("%Y-%m-%d",localtime(os.stat(file).st_birthtime))
+    new_file_name = creation_date+'_'+file
+    print(f'name of file was {file}')
+    if file[:10] != creation_date:
+        os.rename(file, new_file_name)
+        print(f'name of file is now {new_file_name}')
+    else : print('skipped renaming file cause it already has correct naming format')
