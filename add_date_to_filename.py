3497a274281916ba87eca2b04db8dfd7a7aaa1b0
Petur Jokull Thorvaldsson <33139856+peturjokull@users.noreply.github.com>
2021-10-10 22:09:29 +0000

    svona

--- add_date_to_filename.py
+++ add_date_to_filename.py
@@ -1,6 +1,13 @@
 import os
 from time import localtime,strftime
 
+def proceed(_s):
+    check = True
+    for c in _s.replace('-',''):
+        check = c.isnumeric()
+        if check == False : return True
+    return False
+
 path = input(f"Paste your path in here: (C:\\example\\example) \n")
 current_directory = os.getcwd()
 if path == current_directory: pass
@@ -14,7 +21,7 @@ for file in files_in_dir:
     creation_date = strftime("%Y-%m-%d",localtime(os.stat(file).st_birthtime))
     new_file_name = creation_date+'_'+file
     print(f'name of file was {file}')
-    if file[:10] != creation_date:
+    if proceed(file[:10]):
         os.rename(file, new_file_name)
         print(f'name of file is now {new_file_name}')
     else : print('skipped renaming file cause it already has correct naming format')
